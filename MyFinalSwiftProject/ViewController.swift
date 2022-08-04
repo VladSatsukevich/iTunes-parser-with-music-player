2c964f47ba847ed561eb664b95a010be60c42083
VladSatsukevich <dark.knight.de@yandex.ru>
2022-08-04 08:11:57 +0300

    Initial Commit

---
+++ MyFinalSwiftProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFinalSwiftProject
+//
+//  Created by VironIT on 4.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
